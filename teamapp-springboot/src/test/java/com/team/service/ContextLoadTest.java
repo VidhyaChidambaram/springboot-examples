8c1ef6bfd050dc8ef85c88771a8054558952f7cc
vidhya.chidambaram <vidhya.chidambaram@gmail.com>
2017-07-03 22:23:53 -0400

    renamed tests

--- teamapp-springboot/src/test/java/com/team/service/ContextLoadTest.java
+++ teamapp-springboot/src/test/java/com/team/service/ContextLoadTest.java
@@ -1,4 +1,4 @@
-package com.learn.spring;
+package com.team.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MicroSpringbootApplicationTests {
+public class ContextLoadTest {
 
 
 	@Test
